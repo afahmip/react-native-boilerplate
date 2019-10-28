78832b671d13b0618bbf654aff5e14fe5e816175
ahmadfahmip <ahmad.fahmi.pratama@gmail.com>
2019-10-28 15:11:10 +0700

    Rename app

--- android/app/src/main/java/com/reactnative/MainActivity.java
+++ android/app/src/main/java/com/reactnative/MainActivity.java
@@ -1,4 +1,4 @@
-package com.baca;
+package com.reactnative;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -13,7 +13,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "baca";
+    return "ReactNative";
   }
 
   @Override
--- ios/ReactNativeTests/ReactNativeTests.m
+++ ios/ReactNativeTests/ReactNativeTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface bacaTests : XCTestCase
+@interface ReactNativeTests : XCTestCase
 
 @end
 
-@implementation bacaTests
+@implementation ReactNativeTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
